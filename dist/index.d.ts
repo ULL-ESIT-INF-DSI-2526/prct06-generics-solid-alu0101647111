9aa8a67a8b509c793cea9fcad93198986970d266
Lucía Hernández <alu0101647111@ull.edu.es>
2026-03-05 16:51:40 +0000

    Configuration commit

---
+++ dist/index.d.ts
@@ -0,0 +1,31 @@
+/**
+ * Adds two __numbers__
+ * @param firstNumber - Consists of the first operand of the addition
+ * @param secondNumber - Consists of the second operand of the addition
+ * @returns The addition of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * add(1, 7) = 8
+ * ```
+ */
+export declare function add(firstNumber: number, secondNumber: number): number;
+/**
+ * Substracts two __numbers__
+ * @param firstNumber - Consists of the first operand of the substraction
+ * @param secondNumber - Consists of the second operand of the substraction
+ * @returns The substraction of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * sub(1, 7) = -6
+ * ```
+ *
+ */
+export declare function sub(firstNumber: number, secondNumber: number): number;
+/**
+ *
+ * @param firstNumber - Consists of the first operand of the division
+ * @param secondNumber - Consists of the second operand of the division
+ * @returns The division of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * div(4, 8) = 0.5
+ * ```
+ */
+export declare function div(firstNumber: number, secondNumber: number): number;
